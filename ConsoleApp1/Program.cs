342e868b009a7ac52c81bdfbde97bcc1dbcef093
june777 <fea7@naver.com>
2023-04-05 12:56:51 -0400

    aa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!111");
+Console.WriteLine("Hello, World!aa");
 
 Console.WriteLine("Hello, World!");
 
